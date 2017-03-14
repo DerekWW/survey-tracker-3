ffeca4d2f50bd356eabbb38195becac75ec9c270
DerekWW <derek.wierson@gmail.com>
2017-03-14 10:07:17 -0700

    add vue resource

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,18 +1,34 @@
 <template lang="html">
   <div class="Dashboard">
-    {{ user }}
+    <button type="button" v-on:click="getSheet" name="button">get sheet</button>
+    <div class="">
+      {{ spreadsheet }}
+    </div>
   </div>
+
 </template>
 
 <script>
+/*eslint-disable*/
 export default {
   name: 'Dashboard',
   data() {
     return {
-
+      spreadsheet: '',
     };
   },
   props: ['user'],
+  methods: {
+    getSheet: function() {
+      gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: '1tA60Gczx8N2451Ul9_0nPfoMTggHa6k8JWoJQdoJvOI',
+          range: 'Class Data!A2:E',
+        })
+        .then((response) => {
+          this.spreadsheet = response;
+        });
+    },
+  },
 
 };
 </script>
